666103bf94f0e9494af414fb6bcd9c162cbe192e
testDrive64 <com9on@me.com>
2021-12-28 18:53:10 +0100

    can read different cmd's and different priorities

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 #![allow(unused)]
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
 use structopt::StructOpt;
-use std::env;
-use std::str::FromStr;
 use std::string::ParseError;
-use std::io::{BufWriter, Write};
-use std::fs::File;
+use std::str::FromStr;
+use std::path::PathBuf;
+use std::env;
+use std::env::home_dir;
+//use serde::{Deserialize, Serialize};
+//use serde_json::Result;
 
 // Priority of a task, default value is low, option command is -p.
+#[derive(Debug, Copy, Clone)]
 enum Priority {
     Important,
     High,
     Medium,
     Low,
 }
+impl FromStr for Priority {
+    type Err = ParseError;
+
+    fn from_str(prio: &str) -> Result<Self, Self::Err> {
+        match prio {
+            "Important" => Ok(Priority::Important),
+            "High" => Ok(Priority::High),
+            "Medium" => Ok(Priority::Medium),
+            "Low" => Ok(Priority::Low),
+            _ => Ok(Priority::Low), // Err(format!("Could not parse the priority: {}", prio)),
+        }
+    }
+}
 
 // Single task.
+#[derive(Debug)]
 struct Todo {
     task: String,
     priority: Priority,
@@ -24,91 +39,83 @@ struct Todo {
 }
 
 // Controller to open and save the todo list.
-struct Todo_Ctrl {
+#[derive(Debug)]
+struct TodoCtrl {
     data_file: std::path::PathBuf,
     items: Vec<Todo>,
 }
-
-// Command reader, don't know if it is really in need.
-
-#[derive(StructOpt)]
-struct Cli {
-    #[structopt(short = "a", long = "add")]
-    cmd: String,
-    options: Vec<String>,
-}
-
-#[derive(StructOpt)]
-struct Cli_Add {
-    task: String,
-    priority: Priority,
-    datafilename: String,
-}
-impl FromStr for Cli_Add {
-    type Err = ParseError;
-    
-    pub fn from_str(s: &str) -> Result<Self,Self::Err> {
-        let cmd: Vec<&str> = s.split(' ').collect();
-
-        match &s {
-            Low => Ok(Priority::Low);
-            Medium => Ok(Priority::Medium);
-            High => Ok(Priority::High);
-            Important => Ok(Priority::Important);
+impl TodoCtrl {
+    pub fn new(data_file: std::path::PathBuf) -> Self {
+        Self {
+            data_file: data_file,
+            items : Vec::new(),
         }
     }
 }
 
-// Function to handle the income args.
-fn parse_config(args: &[String]) -> (&str, &str) {
-    
-    match &args[1] {
-        Add => println!("CMD: Add"),
-        List => println!("CMD: List"),
-        Edit => println!("CMD: Edit"),
-    }
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+// Command reader, don't know if it is really in need.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "shit_to_do")]
+struct Cli {
+    cmd: String,
+    /// Set a priority 1 - 4, 1 is really important.
+    #[structopt(short, long, default_value = "Low")]
+    priority: Priority,
+    /// Add an new task
+    #[structopt(short, long)]
+    add: Vec<String>,
+    /// List all tasks
+    #[structopt(short, long, default_value = "")]
+    list: String,
+    /// Set status, -s [progress, done].
+    #[structopt(short, long, default_value = "")]
+    status: String,
+
 }
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let cmd = args[1];
-    if args[1] == "All" {
-        let add = Cli_Add::from_args();
-    }
+    let args = Cli::from_args();
 
-    let (query, filename) = parse_config(&args);
-
+    println!("{:#?}", args);
+    let mut datapath = PathBuf::new();
     match env::home_dir() {
-        Some(path) => println!("Your home directory, probably: {}", path.display()),
+        Some(path) => {
+            println!("Your home directory, probably: {}", path.display());
+            datapath = path;
+        },
         None => println!("Impossible to get your home dir!"),
     }
 
-    let cmd = Cli {
-        cmd : query.to_string(),
-        options: Vec::new(),
-    };
+    if datapath.capacity() == 0 {
+        datapath.push("~/.tridos.json");
+    }
+    let mut todo_ctrl = TodoCtrl::new(datapath);
 
-    //let new_task = Todo {
-    //    task: cmd.task,
-    //    priority: Priority::Low,
-    //    id: 1,
-    //};
+    if !args.add.is_empty() {
+        for todo in args.add.iter() {
+            let new_task = Todo {
+                id: 1,
+                task: todo.to_string(),
+                priority: args.priority,
+            }; 
+            todo_ctrl.items.push(new_task);
+        }
+    }
+    
+    println!("{:#?}", todo_ctrl);
 
-    // Create temp file
-    let temp_file = env::temp_dir().join(filename);
+      // Create temp file
+//    let temp_file = env::temp_dir().join(filename);
 
     // Create file if not exists
-    let mut file = File::create(temp_file).unwrap();
-    let mut writer = BufWriter::new(file);
+//    let mut file = File::create(temp_file).unwrap();
+//    let mut writer = BufWriter::new(file);
 
  // let encoded = serde_json::to_string(&new_task).unwrap();
 
  //   println!("{}", encoded);
 
-    println!("Write file ... ");
+//    println!("Write file ... ");
   //  serde_json::to_writer(&mut writer, &new_task)?;
    // writer.flush()?;
     Ok(())
